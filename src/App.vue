ff3469359ba5174a44501cfb6c1d369336797f4f
Jared Potter <jaredpotter1@gmail.com>
2021-08-14 13:47:41 -0700

    Add new forecast component

--- src/App.vue
+++ src/App.vue
@@ -3,65 +3,81 @@
     <img alt="Vue logo" src="./assets/logo.png" />
     <h1>NOAA HRRR SMOKE FORECASTS</h1>
     <div>
-      <button @click="mode = 'sfc_smoke'">Near Surface Smoke</button>
-      <button @click="mode = 'vi_smoke'">Vertically Integrated Smoke</button>
-      <button @click="mode = 'sfc_visibility'">Surface Visibility</button>
+      <button @click="() => setMode('sfc_smoke')">Near Surface Smoke</button>
+      <button @click="() => setMode('vi_smoke')">
+        Vertically Integrated Smoke
+      </button>
+      <button @click="() => setMode('sfc_visibility')">
+        Surface Visibility
+      </button>
     </div>
     <div v-if="mode === 'sfc_smoke'">
       <h3>Near Surface Smoke</h3>
       <div class="forecast-container">
-        <div v-for="forecast in forecasts" :key="forecast.id" class="forecast">
-          <div class="date-time">
-            {{ forecast.timestamp }}
-          </div>
-          <video width="1200" height="1200" controls loop>
-            <source
-              :src="forecast.near_surface_smoke_video_url"
-              type="video/mp4"
-            />
-            <!-- <source src="movie.ogg" type="video/ogg" /> -->
-            Your browser does not support the video tag.
-          </video>
-        </div>
+        <Forecast
+          v-for="forecast in forecasts"
+          :key="forecast.near_surface_smoke_video_url"
+          :timestamp="forecast.timestamp"
+          :url="forecast.near_surface_smoke_video_url"
+        >
+        </Forecast>
       </div>
     </div>
     <div v-if="mode === 'vi_smoke'">
       <h3>Vertically Integrated Smoke</h3>
       <div class="forecast-container">
-        <div v-for="forecast in forecasts" :key="forecast.id" class="forecast">
-          <div class="date-time">
-            {{ forecast.timestamp }}
-          </div>
-          <video width="1200" height="1200" controls loop>
-            <source
-              :src="forecast.vertically_integrated_smoke_video_url"
-              type="video/mp4"
-            />
-            <!-- <source src="movie.ogg" type="video/ogg" /> -->
-            Your browser does not support the video tag.
-          </video>
-        </div>
+        <Forecast
+          v-for="forecast in forecasts"
+          :key="forecast.vertically_integrated_smoke_video_url"
+          :timestamp="forecast.timestamp"
+          :url="forecast.vertically_integrated_smoke_video_url"
+        >
+        </Forecast>
       </div>
     </div>
     <div v-if="mode === 'sfc_visibility'">
       <h3>Surface Visibility</h3>
       <div class="forecast-container">
-        <div v-for="forecast in forecasts" :key="forecast.id" class="forecast">
-          <div class="date-time">
-            {{ forecast.timestamp }}
-          </div>
-          <video width="1200" height="1200" controls loop>
-            <source
-              :src="forecast.surface_visibility_video_url"
-              type="video/mp4"
-            />
-            <!-- <source src="movie.ogg" type="video/ogg" /> -->
-            Your browser does not support the video tag.
-          </video>
-        </div>
+        <Forecast
+          v-for="forecast in forecasts"
+          :key="forecast.surface_visibility_video_url"
+          :timestamp="forecast.timestamp"
+          :url="forecast.surface_visibility_video_url"
+        >
+        </Forecast>
       </div>
     </div>
     <div v-if="isLoading">...LOADING...</div>
+    <h2 v-if="!isLoading && forecasts.length === 0">
+      No Forecasts Found! Let
+      <a href="https://twitter.com/jaredpotter">Jared Know</a>
+    </h2>
+    <div class="credit">
+      <div>
+        Created by <a href="https://twitter.com/jaredpotter">Jared Potter</a>
+      </div>
+      <div>
+        Code Found at:
+        <ul class="links-list">
+          <li>
+            <a
+              href="https://github.com/JaredPotter/noaa-hrrr-smoke-video-generator"
+              >NOAA HRRR Video Generator</a
+            >
+          </li>
+          <li>
+            <a href="https://github.com/JaredPotter/noaa-hrrr-smoke-api"
+              >Laravel API</a
+            >
+          </li>
+          <li>
+            <a href="https://github.com/JaredPotter/noaa-hrrr-smoke-vue-web-app"
+              >Vue Web App</a
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -73,16 +89,24 @@
 // };
 import axios from 'axios';
 import moment from 'moment';
+import Forecast from './components/Forecast.vue';
 
 export default {
   name: 'App',
-  components: {},
+  components: {
+    Forecast,
+  },
   data() {
     return {
       mode: 'sfc_smoke',
       forecasts: [],
       isLoading: false,
     };
+  },
+  methods: {
+    setMode(mode) {
+      this.mode = mode;
+    },
   },
   async mounted() {
     try {
@@ -91,6 +115,8 @@ export default {
         'https://noaa-hrrr-smoke-api.herokuapp.com/forecasts'
         // 'http://localhost:8000/forecasts'
       );
+      // eslint-disable-next-line no-debugger
+      // debugger;
 
       this.forecasts = response.data.data;
 
@@ -110,7 +136,7 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -118,5 +144,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.credit {
+  .links-list {
+    list-style: none;
+  }
 }
 </style>
---
+++ src/components/Forecast.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="forecast-container">
+    <div class="date-time">
+      {{ timestamp }}
+    </div>
+    <video class="video-element" controls loop>
+      <source :src="url" type="video/mp4" />
+      <!-- <source src="movie.ogg" type="video/ogg" /> -->
+      Your browser does not support the video tag.
+    </video>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    timestamp: String,
+    url: String,
+  },
+};
+</script>
+
+<style lang="scss">
+.forecast-container {
+  .video-element {
+    width: 100%;
+  }
+}
+</style>
