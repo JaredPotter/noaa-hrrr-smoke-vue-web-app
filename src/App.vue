0273b316a8d527719c8ad68e80605d42f64d48c7
Jared Potter <jaredpotter1@gmail.com>
2022-09-10 16:29:00 -0600

    Updates...

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <div class="fire-container">
+      <div class="fire">
+        <div class="flames">
+          <div class="flame"></div>
+          <div class="flame"></div>
+          <div class="flame"></div>
+          <div class="flame"></div>
+        </div>
+        <div class="logs"></div>
+      </div>
+    </div>
     <h1>SMOKE 48-HOUR FORECAST (NOAA HRRR)</h1>
     <div>
       Created by <a href="https://twitter.com/jaredpotter">Jared Potter</a>
@@ -8,8 +18,6 @@
     <div class="nav-bar">
       <router-link to="/north-west">North West</router-link> |
       <router-link to="/utah">Utah</router-link> |
-      <router-link to="/colorado">Colorado</router-link> |
-      <router-link to="/new-mexico">New Mexico</router-link> |
       <router-link to="/united-states">United States</router-link>
     </div>
     <router-view />
@@ -50,6 +58,17 @@
 <script>
 export default {
   name: 'App',
+  mounted() {
+    const flames = document.querySelectorAll('.fire .flames .flame');
+
+    setTimeout(() => {
+      flames.forEach((flame) => {
+        // flame.style['animation-play-state'] = 'paus
+        // flame.style['animation'] = '1.5s ease-out';
+        // animation: flameeven 1.5s ease-in infinite;
+      });
+    }, 2000);
+  },
 };
 </script>
 
@@ -81,6 +100,156 @@ body {
 .credit {
   .links-list {
     list-style: none;
+  }
+}
+
+.fire-container {
+  width: 100%;
+  display: flex;
+  // flex-direction: column;
+  justify-content: center;
+}
+
+.fire {
+  height: 12rem;
+  width: 12rem;
+  margin-top: 8rem;
+}
+.fire .flames {
+  width: 60%;
+  height: 60%;
+  transform: translateX(32%) translateY(2%) rotate(45deg);
+}
+.fire .flames .flame {
+  position: absolute;
+  right: 0%;
+  bottom: 0%;
+  width: 0%;
+  height: 0%;
+  background-color: #ffdc01;
+  border-radius: 1vw;
+}
+.fire .flames .flame:nth-child(2n + 1) {
+  animation: flameodd 1.5s ease-in infinite;
+}
+.fire .flames .flame:nth-child(2n) {
+  animation: flameeven 1.5s ease-in infinite;
+}
+@keyframes with_delay {
+  0% {
+    background: red;
+  }
+  75% {
+    background: green;
+  }
+  100% {
+    background: green;
+  }
+}
+
+.fire .flames .flame:nth-child(1) {
+  animation-delay: 0s;
+}
+.fire .flames .flame:nth-child(2) {
+  animation-delay: 0.375s;
+}
+.fire .flames .flame:nth-child(3) {
+  animation-delay: 0.75s;
+}
+.fire .flames .flame:nth-child(4) {
+  animation-delay: 1.125s;
+}
+.fire .logs {
+  bottom: 25%;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 100%;
+  height: 15%;
+}
+.fire .logs:before,
+.fire .logs:after {
+  position: absolute;
+  content: '';
+  top: 50%;
+  left: 50%;
+  transform: rotate(20deg);
+  height: 100%;
+  width: 100%;
+  border-radius: 1vw;
+  background-color: #70392f;
+}
+.fire .logs:before {
+  transform: rotate(-20deg);
+  background-color: #612e25;
+}
+@keyframes flameodd {
+  0%,
+  100% {
+    width: 0%;
+    height: 0%;
+  }
+  25% {
+    width: 100%;
+    height: 100%;
+  }
+  0% {
+    background-color: #ffdc01;
+    z-index: 1000000;
+  }
+  40% {
+    background-color: #fdac01;
+    z-index: 1000000;
+  }
+  100% {
+    background-color: #f73b01;
+    z-index: -10;
+  }
+  0% {
+    right: 0%;
+    bottom: 0%;
+  }
+  25% {
+    right: 1%;
+    bottom: 2%;
+  }
+  100% {
+    right: 150%;
+    bottom: 170%;
+  }
+}
+@keyframes flameeven {
+  0%,
+  100% {
+    width: 0%;
+    height: 0%;
+  }
+  25% {
+    width: 100%;
+    height: 100%;
+  }
+  0% {
+    background-color: #ffdc01;
+    z-index: 1000000;
+  }
+  40% {
+    background-color: #fdac01;
+    z-index: 1000000;
+  }
+  100% {
+    background-color: #f73b01;
+    z-index: -10;
+  }
+  0% {
+    right: 0%;
+    bottom: 0%;
+  }
+  25% {
+    right: 2%;
+    bottom: 1%;
+  }
+  100% {
+    right: 170%;
+    bottom: 150%;
   }
 }
 </style>
