09a1e94188b94f908ad3030bad5aa352318e63b6
Jared Potter <jaredpotter1@gmail.com>
2022-09-10 22:33:45 -0600

    Update Page. Add Time Ago

--- src/components/Forecast.vue
+++ src/components/Forecast.vue
@@ -1,22 +1,40 @@
 <template>
   <div class="forecast-container">
-    <div v-if="isLoading"><div class="lds-dual-ring"></div></div>
+    <div v-if="isLoading" class="fire-container">
+      <div class="fire">
+        <div class="flames">
+          <div class="flame"></div>
+          <div class="flame"></div>
+          <div class="flame"></div>
+          <div class="flame"></div>
+        </div>
+        <div class="logs"></div>
+      </div>
+    </div>
     <h2 v-if="!isLoading && !currentForecast">
       No Forecasts Found! Let
       <a href="https://twitter.com/jaredpotter">Jared</a> Know
     </h2>
     <div v-if="currentForecast" class="current-forecast-container">
-      <button class="button" @click="() => setMode('sfc_smoke')">
+      <button
+        class="button"
+        :class="{ 'button-selected button': mode === 'sfc_smoke' }"
+        @click="() => setMode('sfc_smoke')"
+      >
         Near Surface Smoke
       </button>
-      <button class="button" @click="() => setMode('vi_smoke')">
+      <button
+        class="button"
+        :class="{ 'button-selected button': mode === 'vi_smoke' }"
+        @click="() => setMode('vi_smoke')"
+      >
         Vertically Integrated Smoke
       </button>
-      <div class="date-time">
-        {{ timestamp }}
-      </div>
-      <div v-if="mode === 'sfc_smoke'">
-        <h3>Near Surface Smoke - Latest Forecast</h3>
+      <h3>
+        Latest Forecast - Last Updated {{ timeAgo }} (updates every ~6 hours)
+      </h3>
+      <div v-if="mode === 'sfc_smoke' && !isLoading">
+        <h3>Near Surface Smoke</h3>
         <div class="forecast-container">
           <video
             class="video-element"
@@ -46,8 +64,8 @@
           </video>
         </div>
       </div>
-      <div v-if="mode === 'vi_smoke'">
-        <h3>Vertically Integrated Smoke - Latest Forecast</h3>
+      <div v-if="mode === 'vi_smoke' && !isLoading">
+        <h3>Vertically Integrated Smoke</h3>
         <div class="forecast-container">
           <video
             class="video-element"
@@ -96,6 +114,7 @@ import {
   getDocs,
   where,
   query,
+  orderBy,
 } from 'firebase/firestore/lite';
 
 const db = getFirestore(app);
@@ -106,6 +125,7 @@ export default {
       mode: 'sfc_smoke',
       currentForecast: null,
       isLoading: false,
+      timeAgo: null,
     };
   },
   watch: {
@@ -131,7 +151,11 @@ export default {
         this.isLoading = true;
 
         const forecastsCollectionRef = collection(db, 'forecasts');
-        const q = query(forecastsCollectionRef, where('areaCode', '==', area));
+        let q = query(
+          forecastsCollectionRef,
+          orderBy('timestamp', 'desc'),
+          where('areaCode', '==', area)
+        );
         const querySnapshot = await getDocs(q);
 
         const forecasts = [];
@@ -146,9 +170,14 @@ export default {
           this.$router.push('/');
         } else {
           this.currentForecast = forecasts[0];
-          this.currentForecast.timestamp = moment(
-            this.currentForecast.timestamp
-          ).format('dddd MMMM Do, h:mma');
+          const timestamp = this.currentForecast.timestamp;
+          const momentTimestamp = moment.unix(timestamp);
+
+          this.formattedTimestamp = momentTimestamp.format(
+            'yyyy dddd MMMM Do, h:mma'
+          );
+          // this.timeAgo = new moment(this.currentForecast.timestamp).fromNow();
+          this.timeAgo = momentTimestamp.fromNow();
         }
       } catch (error) {
         this.isLoading = false;
@@ -156,7 +185,9 @@ export default {
         throw error;
       }
 
-      this.isLoading = false;
+      setTimeout(() => {
+        this.isLoading = false;
+      }, 1000);
     },
   },
   props: {
@@ -181,6 +212,12 @@ export default {
     cursor: pointer;
   }
 
+  .button-selected {
+    transform: scale(0.98);
+    box-shadow: 3px 2px 22px 1px rgba(0, 0, 0, 0.24);
+    opacity: 0.75;
+  }
+
   .button:hover {
     opacity: 0.75;
   }
@@ -190,28 +227,153 @@ export default {
   }
 }
 
-.lds-dual-ring {
-  display: inline-block;
-  width: 80px;
-  height: 80px;
+.fire-container {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding-bottom: 3rem;
 }
-.lds-dual-ring:after {
-  content: ' ';
-  display: block;
-  width: 64px;
-  height: 64px;
-  margin: 8px;
-  border-radius: 50%;
-  border: 6px solid #41b883;
-  border-color: #41b883 transparent #41b883 transparent;
-  animation: lds-dual-ring 1.2s linear infinite;
+
+.fire {
+  height: 12rem;
+  width: 12rem;
+  margin-top: 8rem;
 }
-@keyframes lds-dual-ring {
+.fire .flames {
+  width: 60%;
+  height: 60%;
+  transform: translateX(32%) translateY(2%) rotate(45deg);
+}
+.fire .flames .flame {
+  position: absolute;
+  right: 0%;
+  bottom: 0%;
+  width: 0%;
+  height: 0%;
+  background-color: #ffdc01;
+  border-radius: 1vw;
+}
+.fire .flames .flame:nth-child(2n + 1) {
+  animation: flameodd 1.5s ease-in infinite;
+}
+.fire .flames .flame:nth-child(2n) {
+  animation: flameeven 1.5s ease-in infinite;
+}
+@keyframes with_delay {
   0% {
-    transform: rotate(0deg);
+    background: red;
+  }
+  75% {
+    background: green;
   }
   100% {
-    transform: rotate(360deg);
+    background: green;
+  }
+}
+
+.fire .flames .flame:nth-child(1) {
+  animation-delay: 0s;
+}
+.fire .flames .flame:nth-child(2) {
+  animation-delay: 0.375s;
+}
+.fire .flames .flame:nth-child(3) {
+  animation-delay: 0.75s;
+}
+.fire .flames .flame:nth-child(4) {
+  animation-delay: 1.125s;
+}
+.fire .logs {
+  bottom: 25%;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 100%;
+  height: 15%;
+}
+.fire .logs:before,
+.fire .logs:after {
+  position: absolute;
+  content: '';
+  top: 50%;
+  left: 50%;
+  transform: rotate(20deg);
+  height: 100%;
+  width: 100%;
+  border-radius: 1vw;
+  background-color: #70392f;
+}
+.fire .logs:before {
+  transform: rotate(-20deg);
+  background-color: #612e25;
+}
+@keyframes flameodd {
+  0%,
+  100% {
+    width: 0%;
+    height: 0%;
+  }
+  25% {
+    width: 100%;
+    height: 100%;
+  }
+  0% {
+    background-color: #ffdc01;
+    z-index: 1000000;
+  }
+  40% {
+    background-color: #fdac01;
+    z-index: 1000000;
+  }
+  100% {
+    background-color: #f73b01;
+    z-index: -10;
+  }
+  0% {
+    right: 0%;
+    bottom: 0%;
+  }
+  25% {
+    right: 1%;
+    bottom: 2%;
+  }
+  100% {
+    right: 150%;
+    bottom: 170%;
+  }
+}
+@keyframes flameeven {
+  0%,
+  100% {
+    width: 0%;
+    height: 0%;
+  }
+  25% {
+    width: 100%;
+    height: 100%;
+  }
+  0% {
+    background-color: #ffdc01;
+    z-index: 1000000;
+  }
+  40% {
+    background-color: #fdac01;
+    z-index: 1000000;
+  }
+  100% {
+    background-color: #f73b01;
+    z-index: -10;
+  }
+  0% {
+    right: 0%;
+    bottom: 0%;
+  }
+  25% {
+    right: 2%;
+    bottom: 1%;
+  }
+  100% {
+    right: 170%;
+    bottom: 150%;
   }
 }
 </style>
